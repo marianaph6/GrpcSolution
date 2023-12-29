b90cad69f99d2561b6575a6ee2cfe2db8ddef6e0
marianaph6 <mariana1996.mph@gmail.com>
2023-12-29 12:15:56 -0500

    Implementación del servicio ReadProduct

--- src/GrpcMantenimiento/Services/ProductService.cs
+++ src/GrpcMantenimiento/Services/ProductService.cs
@@ -1,6 +1,7 @@
 using Grpc.Core;
 using GrpcMantenimiento.Data;
 using GrpcMantenimiento.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace GrpcMantenimiento.Services
 {
@@ -40,6 +41,36 @@ namespace GrpcMantenimiento.Services
                 }
             );
         }
+    
+        public override async Task<ReadProductResponse> ReadProduct (ReadProductRequest readProductRequest, ServerCallContext context)
+        {
+            if (string.IsNullOrEmpty(readProductRequest.Id.ToString()))
+            {
+                throw new RpcException(new Status(StatusCode.InvalidArgument,"Ingrese un Id de valido para un producto"));
+            }
+
+            if(readProductRequest.Id <= 0)
+            {
+                throw new RpcException(new Status(StatusCode.InvalidArgument, "El indice del Id debe der mayor a 0"));
+            }
+
+            //Product product = await _dbContext.Products.FindAsync(readProductRequest.Id);
+
+            Product product = await _dbContext.Products.FirstOrDefaultAsync(p => p.Id == readProductRequest.Id) ?? throw new RpcException(new Status(StatusCode.NotFound,$"El producto con el Id {readProductRequest.Id} no existe"));
+            return await Task.FromResult(
+                new ReadProductResponse
+                {
+                    Id= product.Id,
+                    Name = product.Name,
+                    Description = product.Description,
+                    Status = product.Status,
+                   
+                }
+                );
+
+        }
+
+
     }
 
     
