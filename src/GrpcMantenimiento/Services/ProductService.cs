eefa26e819f6f2121e616d172e14c89ae7636bd4
Mariana Palacios Hinestroza <mpalacios@sistecredito.com>
2023-12-29 08:47:52 -0500

    Implementacion de CreateProduct

--- src/GrpcMantenimiento/Services/ProductService.cs
+++ src/GrpcMantenimiento/Services/ProductService.cs
@@ -1,12 +1,46 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Grpc.Core;
+using GrpcMantenimiento.Data;
+using GrpcMantenimiento.Models;
 
 namespace GrpcMantenimiento.Services
 {
-    public class ProductService                       
+    public class ProductService   : ProducIT.ProducITBase                    
     {
-        
+        private readonly GrpcDbContext _dbContext;
+
+        public ProductService(GrpcDbContext dbContext) => _dbContext = dbContext;
+
+        public override async Task<CreateProductResponse> CreateProduct( CreateProductRequest createProductRequest, ServerCallContext context)
+        {
+            // Validar campos
+            if(string.IsNullOrEmpty(createProductRequest.Name) || string.IsNullOrEmpty(createProductRequest.Description))
+            {
+                throw new RpcException(new Status(StatusCode.InvalidArgument, "Envie los datos correctamente"));
+            }
+
+            //Crear objeto para el db context
+            Product product = new()
+            {
+                Name = createProductRequest.Name,
+                Description = createProductRequest.Description
+            };
+
+            // Crear Producto
+            await _dbContext.AddAsync(product); 
+            
+            // Guardar y confirmar cambios en el dbContext
+            await _dbContext.SaveChangesAsync(); 
+
+            // Retornar la data segun el la estructura del proto
+            return await Task.FromResult(
+
+                new CreateProductResponse
+                {
+                    Id = product.Id
+                }
+            );
+        }
     }
+
+    
 }
