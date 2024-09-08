cd92ffc43aa607f7b7dc20eee7c45362092a69fd
Mariana Palacios <82735874+marianaph6@users.noreply.github.com>
2024-09-08 18:58:38 -0500

    Update GreeterService.cs

--- src/GrpcMantenimiento/Services/GreeterService.cs
+++ src/GrpcMantenimiento/Services/GreeterService.cs
@@ -13,6 +13,7 @@ public class GreeterService : Greeter.GreeterBase
 
     public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
     {
+        var Password = "admin123";
         return Task.FromResult(new HelloReply
         {
             Message = "Hello " + request.Name
